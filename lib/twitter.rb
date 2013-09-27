3d0e51928df5f1982a1b45be9d8e7c2d9a03e383
Matt Atkins <matt@yoomee.com>
2013-09-27 11:41:55 +0100

    Fix for Twitter API changes

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,11 +1,11 @@
 class Twitter
   include HTTParty
-  base_uri 'http://search.twitter.com'
+  base_uri 'https://api.twitter.com'
   
   Twitter::BLACK_LIST = File.readlines("#{Rails.root}/lib/blacklist.txt").map{|w| w.strip.downcase}
   
   def self.search(term)
-    tweets = get('/search.json', :query => {:q => URI.encode(term), :rpp => 30})["results"]
+    tweets = get('/1.1/search/tweets.json', :query => {:q => URI.encode(term), :rpp => 30}, :headers => {"Authorization" => "Bearer AAAAAAAAAAAAAAAAAAAAAE6ORQAAAAAAIHgBvYwKX6VNYCvOAopBkW4XGl8%3D0wGL20mu9A5u12JvTMhsdbteFi77SPOnE1wURuKTjA"})["statuses"] || []
     tweets.reject do |tweet|
       tweet["text"].split(/[^\w]/).any?{|word| Twitter::BLACK_LIST.include?(word.downcase)}
     end
